0d19ec1311d40221ac5a0310b20953484c4974b8
Andras <andras@acm.org>
2014-10-21 21:24:42 -0400

    adjust phpdate 'c' and 'r' conversions, fix broken phpunit unit test; 0.0.16

--- test/test-phpdate.js
+++ test/test-phpdate.js
@@ -3,11 +3,31 @@ phpdate = require('../index').phpdate;
 module.exports = {
     setUp: function(done) {
         this.now = Date.now();
+        this.timestamp = 315682496123;          // 1980-01-02 12:34:56.123000
+        done();
     },
 
-    'should return the year': function(t) {
-        var str = phpdate('Y', 86400000);
-        t.equal('1970', str);
+    'should report on timestamp number': function(t) {
+        var str = phpdate('Y-m-d', 86400000/2);
+        t.equal('1970-01-01', str);
+        t.done();
+    },
+
+    'should report on timestamp object': function(t) {
+        var str = phpdate('Y-m-d', new Date(86400000/2));
+        t.equal('1970-01-01', str);
+        t.done();
+    },
+
+    'should format Y-m-d H:i:s.u': function(t) {
+        var str = phpdate('Y-m-d H:i:s.u', 315682496123);
+        t.equal('1980-01-02 12:34:56.123000', str);
+        t.done();
+    },
+
+    'should format c': function(t) {
+        var str = phpdate('c', 315682496123);
+        t.equal('1980-01-02T12:34:56-05:00', str);
         t.done();
     },
 
